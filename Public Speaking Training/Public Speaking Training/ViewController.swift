a402099ba87d6db6b619f22e587c6c60ad865142
pwehbe <pwehbe@atheerair.com>
2018-11-18 07:49:28 -0800

    PDf Viewer and Navigation

--- Public Speaking Training/Public Speaking Training/ViewController.swift
+++ Public Speaking Training/Public Speaking Training/ViewController.swift
@@ -1,20 +1,54 @@
-//
-//  ViewController.swift
-//  Public Speaking Training
-//
-//  Created by Pierre WEHBE on 11/18/18.
-//  Copyright © 2018 Pierre Wehbe. All rights reserved.
-//
-
+import PDFKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    public var fileLocalPath: String!
+    public var data: Data? = nil
+    public var pdfView: PDFView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addTap()
+        setUpView()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    private func setUpView() {
+        pdfView = PDFView(frame: view.frame)
+        pdfView.center = view.center
+        let path = Bundle.main.path(forResource: "test", ofType: "pdf")
+        let url = URL(fileURLWithPath: path!)
+        let pdfDocument = PDFDocument(url: url)
+        pdfView.document = pdfDocument
+        pdfView.displayDirection = .horizontal
+        pdfView.displayMode = .singlePage
+        pdfView.autoScales = true
+        view.addSubview(pdfView)
+    }
+    
+    private func addTap() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+        if let xTouchPos = sender?.location(in: view).x {
+            if xTouchPos > view.center.x {
+                if pdfView.canGoToNextPage() {
+                   pdfView.goToNextPage(sender)
+                }
+            } else {
+                if pdfView.canGoBack() {
+                    pdfView.goToPreviousPage(sender)
+                }
+            }
+        }
+        
+    }
 
 }
 
