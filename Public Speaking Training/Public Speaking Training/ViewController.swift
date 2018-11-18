3bad8c0808e266659bbfd19b3f80c11294aafba9
pwehbe <pwehbe@atheerair.com>
2018-11-18 13:40:10 -0800

    Recorders and Speech to text run in //

--- Public Speaking Training/Public Speaking Training/ViewController.swift
+++ Public Speaking Training/Public Speaking Training/ViewController.swift
@@ -1,8 +1,11 @@
 import AVKit
 import PDFKit
 import UIKit
+import googleapis
 
 class ViewController: UIViewController {
+    
+    private let SAMPLE_RATE = 16000
     
     public var fileLocalPath: String!
     public var data: Data? = nil
@@ -10,14 +13,13 @@ class ViewController: UIViewController {
     
     public var currentInstance: PresentationInstance!
     
+    var audioData: NSMutableData!
+    
     @IBOutlet weak var doneButton: UIButton!
-    @IBAction func donePresenting(_ sender: Any) {
-        currentInstance.stopAll()
-    }
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        AudioController.sharedInstance.delegate = self
         addTap()
         setUpView()
     }
@@ -38,7 +40,7 @@ class ViewController: UIViewController {
         view.addSubview(pdfView)
         view.bringSubviewToFront(doneButton)
         currentInstance.delegate = self
-        currentInstance.startRecording()
+        startRecording()
     }
     
     private func addTap() {
@@ -62,11 +64,71 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    //MARK: Button Actions
+    @IBAction func donePresenting(_ sender: Any) {
+        _ = AudioController.sharedInstance.stop()
+        SpeechRecognitionService.sharedInstance.stopStreaming()
+        currentInstance.stopAll()
+    }
 }
 
 extension ViewController: AVAudioRecorderDelegate {
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
         let path = recorder.url
         print(path)
+    }
+    
+    func startRecording() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.record, mode: .default)
+        } catch {
+            print("Coudn't start recording")
+        }
+        audioData = NSMutableData()
+        _ = AudioController.sharedInstance.prepare(specifiedSampleRate: SAMPLE_RATE)
+        SpeechRecognitionService.sharedInstance.sampleRate = SAMPLE_RATE
+        _ = AudioController.sharedInstance.start()
+        currentInstance.startRecording()
+    }
+}
+
+extension ViewController: AudioControllerDelegate {
+    func processSampleData(_ data: Data) -> Void {
+        audioData.append(data)
+        
+        // We recommend sending samples in 100ms chunks
+        let chunkSize : Int /* bytes/chunk */ = Int(0.1 /* seconds/chunk */
+            * Double(SAMPLE_RATE) /* samples/second */
+            * 2 /* bytes/sample */);
+        
+        if (audioData.length > chunkSize) {
+            SpeechRecognitionService.sharedInstance.streamAudioData(audioData,
+                                                                    completion:
+                { [weak self] (response, error) in
+                    guard let strongSelf = self else {
+                        return
+                    }
+                    
+                    if let error = error {
+                        print("Error in Google API")
+                    } else if let response = response {
+                        var finished = false
+                        for result in response.resultsArray! {
+                            if let result = result as? StreamingRecognitionResult {
+                                if result.isFinal {
+                                    print(response)
+                                    finished = true
+                                }
+                            }
+                        }
+                        if finished {
+                            //strongSelf.stopAudio(strongSelf)
+                        }
+                    }
+            })
+            self.audioData = NSMutableData()
+        }
     }
 }
--- Public Speaking Training/google/longrunning/Operations.pbrpc.h
+++ Public Speaking Training/google/longrunning/Operations.pbrpc.h
@@ -1,5 +1,5 @@
 #if !defined(GPB_GRPC_FORWARD_DECLARE_MESSAGE_PROTO) || !GPB_GRPC_FORWARD_DECLARE_MESSAGE_PROTO
-#import "google/longrunning/Operations.pbobjc.h"
+#import <googleapis/Operations.pbobjc.h>
 #endif
 
 #if !defined(GPB_GRPC_PROTOCOL_ONLY) || !GPB_GRPC_PROTOCOL_ONLY
@@ -18,7 +18,7 @@
 @class Operation;
 
 #if !defined(GPB_GRPC_FORWARD_DECLARE_MESSAGE_PROTO) || !GPB_GRPC_FORWARD_DECLARE_MESSAGE_PROTO
-  #import "google/api/Annotations.pbobjc.h"
+  #import <googleapis/Annotations.pbobjc.h>
 #if defined(GPB_USE_PROTOBUF_FRAMEWORK_IMPORTS) && GPB_USE_PROTOBUF_FRAMEWORK_IMPORTS
   #import <Protobuf/Any.pbobjc.h>
 #else
@@ -29,7 +29,7 @@
 #else
   #import "google/protobuf/Empty.pbobjc.h"
 #endif
-  #import "google/rpc/Status.pbobjc.h"
+  #import <googleapis/Status.pbobjc.h>
 #endif
 
 @class GRPCProtoCall;
