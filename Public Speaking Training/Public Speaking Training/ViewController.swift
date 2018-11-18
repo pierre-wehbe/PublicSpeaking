9599f62d545cce06017bde668406413483bc7771
pwehbe <pwehbe@atheerair.com>
2018-11-18 07:21:36 -0800

    Project Init

---
+++ Public Speaking Training/Public Speaking Training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Public Speaking Training
+//
+//  Created by Pierre WEHBE on 11/18/18.
+//  Copyright © 2018 Pierre Wehbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
